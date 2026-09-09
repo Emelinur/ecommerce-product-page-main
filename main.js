51758f963cc1d43af1ad04e69f93d9b46b6efe13
Emelinur <emelinuryzgc@gmail.com>
2026-09-09 17:55:47 +0300

    HTML, CSS, and JS updates: Enhanced lightbox modal functionality with improved image navigation and display, and refined styles for better user experience.

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 const productThumbnailBtn=document.querySelectorAll(".product__thumb-btn");
-const productMainImg=document.querySelector(".product__main-img")
+const productMainImg=document.querySelector(".product__main-img");
+const lightbox=document.querySelector(".lightbox");
+const lightboxBtnNext=document.querySelector(".lightbox__btn--next");
+const lightboxMainImg=document.querySelector(".lightbox__main-img");
+const lightboxThumbBtn=document.querySelectorAll(".lightbox__thumb-btn")
+
+
 productThumbnailBtn.forEach((btn,index)=>{
   btn.addEventListener("click",(e)=>{
   if(e.isTrusted){
-    console.log(productMainImg.src)
 productMainImg.src=`images/image-product-${index + 1}.jpg`
 productThumbnailBtn.forEach((b) => b.classList.remove("product__thumb-btn--active"));
     btn.classList.add("product__thumb-btn--active");
   }
   })
 })
+productMainImg.addEventListener("click",()=>{
+lightbox.style.display="flex";
+lightboxBtnNext.addEventListener("click",()=>{
+ lightboxThumbBtn.forEach((btn,index)=>{
+  btn.addEventListener("click",(a)=>{
+console.log(a)
+ if(a.isTrusted){
+lightboxMainImg.src=`images/image-product-${index + 1}.jpg`
+lightboxBtnNext.forEach((c) => c.classList.remove("product__thumb-btn--active"));
+    btn.classList.add("product__thumb-btn--active");
+  }
+  })
+ })
+})
+})
+
