1a266d9f3c41dba9418933c1fecf3b56a00b86af
Emelinur <emelinuryzgc@gmail.com>
2026-09-10 17:55:21 +0300

    JavaScript updates: Added lightbox thumbnail query selection and improved event handling for thumbnail activation in the lightbox.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const lightbox=document.querySelector(".lightbox");
 const lightboxBtnNext=document.querySelector(".lightbox__btn--next");
 const lightboxMainImg=document.querySelector(".lightbox__main-img");
 const lightboxThumbBtn=document.querySelectorAll(".lightbox__thumb-btn")
-
+const lightboxThumbnails=document.querySelector(".lightbox__thumbnails")
 
 productThumbnailBtn.forEach((btn,index)=>{
   btn.addEventListener("click",(e)=>{
@@ -15,19 +15,24 @@ productThumbnailBtn.forEach((b) => b.classList.remove("product__thumb-btn--activ
   }
   })
 })
+ lightboxBtnNext.addEventListener("click",()=>{
+  
+for(let i=0; i<lightboxThumbBtn.length; i++){
+console.log(i)
+}
+  })
 productMainImg.addEventListener("click",()=>{
 lightbox.style.display="flex";
-lightboxBtnNext.addEventListener("click",()=>{
  lightboxThumbBtn.forEach((btn,index)=>{
   btn.addEventListener("click",(a)=>{
-console.log(a)
  if(a.isTrusted){
 lightboxMainImg.src=`images/image-product-${index + 1}.jpg`
-lightboxBtnNext.forEach((c) => c.classList.remove("product__thumb-btn--active"));
-    btn.classList.add("product__thumb-btn--active");
+lightboxThumbBtn.forEach((c) => c.classList.remove("lightbox__thumb-btn--active"));
+    btn.classList.add("lightbox__thumb-btn--active");
   }
   })
  })
+
 })
-})
+ 
 
